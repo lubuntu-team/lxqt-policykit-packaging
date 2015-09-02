5e3eb9616723664693e70571890020248a5c17b2
Alf Gaida <agaida@siduction.org>
2015-09-03 00:22:41 +0200

    Adding upstream version 0.9.0+20150830.

--- translations/lxqt-policykit-agent_de.ts
+++ translations/lxqt-policykit-agent_de.ts
@@ -1,33 +1,33 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
+<TS version="2.1" language="de">
 <context>
     <name>LXQtPolicykit::PolicykitAgent</name>
     <message>
         <location filename="../src/policykitagent.cpp" line="71"/>
         <location filename="../src/policykitagent.cpp" line="149"/>
         <source>PolicyKit Information</source>
-        <translation type="unfinished"></translation>
+        <translation>PolicyKit Information</translation>
     </message>
     <message>
         <location filename="../src/policykitagent.cpp" line="71"/>
         <source>Another authentization in progress. Please try it again later.</source>
-        <translation type="unfinished"></translation>
+        <translation>Eine andere Authentisierung wird gerade durchgeführt. Bitte später noch einmal versuchen.</translation>
     </message>
     <message>
         <location filename="../src/policykitagent.cpp" line="132"/>
         <source>Authorization Failed</source>
-        <translation type="unfinished"></translation>
+        <translation>Autorisierung fehlgeschlagen</translation>
     </message>
     <message>
         <location filename="../src/policykitagent.cpp" line="132"/>
         <source>Authorization failed for some reason</source>
-        <translation type="unfinished"></translation>
+        <translation>Autorisierung aus irgendeinem Grund fehlgeschlagen</translation>
     </message>
     <message>
         <location filename="../src/policykitagent.cpp" line="144"/>
         <source>PolicyKit Error</source>
-        <translation type="unfinished"></translation>
+        <translation>PolicyKit Fehler</translation>
     </message>
 </context>
 <context>
@@ -35,17 +35,17 @@
     <message>
         <location filename="../src/policykitagentgui.ui" line="14"/>
         <source>Authentication Required</source>
-        <translation type="unfinished"></translation>
+        <translation>Authentisierung benötigt</translation>
     </message>
     <message>
         <location filename="../src/policykitagentgui.ui" line="55"/>
         <source>Identity:</source>
-        <translation type="unfinished"></translation>
+        <translation>Identität:</translation>
     </message>
     <message>
         <location filename="../src/policykitagentgui.ui" line="65"/>
         <source>Password:</source>
-        <translation type="unfinished"></translation>
+        <translation>Kennwort:</translation>
     </message>
 </context>
 </TS>
--- translations/lxqt-policykit-agent_el.ts
+++ translations/lxqt-policykit-agent_el.ts
@@ -1,33 +1,33 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
+<TS version="2.1" language="el">
 <context>
     <name>LXQtPolicykit::PolicykitAgent</name>
     <message>
         <location filename="../src/policykitagent.cpp" line="71"/>
         <location filename="../src/policykitagent.cpp" line="149"/>
         <source>PolicyKit Information</source>
-        <translation type="unfinished"></translation>
+        <translation>Πληροφορίες PolicyKit</translation>
     </message>
     <message>
         <location filename="../src/policykitagent.cpp" line="71"/>
         <source>Another authentization in progress. Please try it again later.</source>
-        <translation type="unfinished"></translation>
+        <translation>Μια άλλη ταυτοποίηση είναι σε εξέλιξη. Παρακαλώ προσπαθήστε ξανά αργότερα.</translation>
     </message>
     <message>
         <location filename="../src/policykitagent.cpp" line="132"/>
         <source>Authorization Failed</source>
-        <translation type="unfinished"></translation>
+        <translation>Η ταυτοποίηση απέτυχε</translation>
     </message>
     <message>
         <location filename="../src/policykitagent.cpp" line="132"/>
         <source>Authorization failed for some reason</source>
-        <translation type="unfinished"></translation>
+        <translation>Η ταυτοποίηση για κάποια αιτία απέτυχε</translation>
     </message>
     <message>
         <location filename="../src/policykitagent.cpp" line="144"/>
         <source>PolicyKit Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Σφάλμα PolicyKit</translation>
     </message>
 </context>
 <context>
@@ -35,17 +35,17 @@
     <message>
         <location filename="../src/policykitagentgui.ui" line="14"/>
         <source>Authentication Required</source>
-        <translation type="unfinished"></translation>
+        <translation>Απαιτείται ταυτοποίηση</translation>
     </message>
     <message>
         <location filename="../src/policykitagentgui.ui" line="55"/>
         <source>Identity:</source>
-        <translation type="unfinished"></translation>
+        <translation>Ταυτότητα:</translation>
     </message>
     <message>
         <location filename="../src/policykitagentgui.ui" line="65"/>
         <source>Password:</source>
-        <translation type="unfinished"></translation>
+        <translation>Κωδικός πρόσβασης:</translation>
     </message>
 </context>
 </TS>
